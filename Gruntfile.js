0cc2a6599d21d8db33327d867f901ad03f8a8ee7
Ben Parker <ben@mail24.co.uk>
2013-07-01 13:03:01 +0100

    Add grunt watch plugin for quicker development

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,15 @@
 module.exports = function(grunt) {
+    /**
+     * Load plugins
+     */
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('default', ['sass', 'concat']);
+
+    /**
+     * Grunt tasks config
+     */
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
@@ -19,10 +30,19 @@ module.exports = function(grunt) {
                 ],
                 dest: 'web/js/polyfills.js'
             }
+        },
+        watch: {
+            scss: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('default', ['sass', 'concat']);
+    /**
+     * Log 'watch' event changes
+     */
+    grunt.event.on('watch', function(action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
 };
